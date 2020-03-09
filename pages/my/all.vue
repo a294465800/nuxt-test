136ba6420ce13bc7d6f962d4a769cb9b2ab70440
曾诗永 <jack7770126@gmail.com>
2020-03-09 19:08:36 +0800

    first commit

---
+++ pages/my/all.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container" align="center">
+    <a href="#a" style="font-size: 60px;">
+      url anchor #a
+    </a>
+
+    <div style="margin-top: 20px;">
+      <p>expected: http://localhost:3000/test/my/all#a</p>
+      <p>actually: http://localhost:3000/test/#a</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='less'>
+</style>
